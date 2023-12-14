449ca9bc3a9f20645eb0d851ad1fc5debfe0024d
Aman Islam <152940423+amanislamwork@users.noreply.github.com>
2023-12-14 02:55:56 +0000

    new commit

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,30 @@
+"use strict";  //treat all Js code as a newer version
+
+// alert(3+3)  //we are using nodejs, not browser
+
+// code readablilty shoulb be high
+
+let name = "aman"
+let age = 18
+let isLogged = true
+
+console.table([
+    name,
+    age,
+    isLogged
+])
+//
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
